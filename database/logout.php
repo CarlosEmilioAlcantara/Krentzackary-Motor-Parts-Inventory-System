91e31ccf19962af23e904a7ff0396acd0bbd8ce3
Carlos Alcantara <agq.xyz@gmail.com>
2023-11-04 00:07:19 +0800

    Now when user logs out, goes back to login page

--- database/logout.php
+++ database/logout.php
@@ -4,10 +4,13 @@
     // Unset all variables from the session: $user, $stmt, $username, etc.
     session_unset();
 
-    // Just completely remove the session
-    session_destroy();
+    // Go straight back to Login page once logged out
+    if (!isset($_SESSION['user'])) header('Location: ../php/login.php');
+
+    // // Just completely remove the session
+    // session_destroy();
 ?>
 
-<html>
+<!-- <html>
     <p>huzzah!</p>
-</html>
+</html> -->
