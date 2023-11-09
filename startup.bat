00a3c2dabea91437d3801f4a3ac30de8405ee916
Carlos Alcantara <agq.xyz@gmail.com>
2023-11-09 22:47:59 +0800

    Added bat file for automatic startup

---
+++ startup.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+set chrome=C:\Program Files\Google\Chrome\Application\chrome.exe
+::set xampp=D:\xampp\xampp-control.exe
+
+start D:\xampp\xampp-control.exe && start chrome http://localhost/krentzackary/php/login.php
