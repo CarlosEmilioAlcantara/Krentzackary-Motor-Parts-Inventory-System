ff460caf890bb42a0db3dbc382adf82e1264fde2
Carlos Alcantara <agq.xyz@gmail.com>
2023-11-17 01:02:45 +0800

    Added delete product popup (needs fixing)

--- js/products-script.js
+++ js/products-script.js
@@ -1,15 +1,22 @@
 const editButton = document.querySelector(".products-edit-button");
 const editPopup = document.querySelector(".popup-edit-product");
 const deleteButton = document.querySelector(".products-delete-button");
+const deletePopup = document.querySelector(".popup-delete-product");
 const overlay = document.querySelector(".overlay");
-const exit = document.querySelector(".popup-edit-header__exit");
+const exit = document.querySelector(".popup__header-exit");
 
 editButton.addEventListener("click", () => {
     overlay.classList.toggle("open");
     editPopup.classList.toggle("open");
 })
 
+deleteButton.addEventListener("click", () => {
+    overlay.classList.toggle("open");
+    deletePopup.classList.toggle("open");
+})
+
 exit.addEventListener("click", () => {
     overlay.classList.toggle("open");
     editPopup.classList.toggle("open");
+    deletePopup.classList.toggle("open");
 })
